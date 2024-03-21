b90c0bb2516f2135389c2b586acd1b2fa6d5d7e0
Ivan D Vasin <ivan@nisavid.io>
2024-03-21 13:13:55 -0400

    fix(sendContactFormEmail): set mail headers so as to appease Mailtrap
    Do what works instead of what's right.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,8 +3,7 @@ import { HttpsError, onCall } from "firebase-functions/v2/https";
 import * as Logger from "firebase-functions/logger";
 import * as Params from "firebase-functions/params";
 
-import * as Nodemailer from "nodemailer";
-import { MailtrapTransport as mailtrapTransport } from "mailtrap";
+import { Mail, MailtrapClient } from "mailtrap";
 
 Admin.initializeApp();
 
@@ -27,38 +26,60 @@ export const sendContactFormEmail = onCall(
     secrets: [mailtrapToken],
   },
   async (request) => {
-    const transport = Nodemailer.createTransport(mailtrapTransport({
-      token: mailtrapToken.value(),
-    }));
+    Logger.info(
+      `Received request to send message from ${request.data.name} <${request.data.email}>:`,
+      request.data.subject,
+    );
 
-    try {
-      await transport.verify();
-    } catch (err) {
-      if (err) {
-        throw new HttpsError("failed-precondition", err.toString(), err);
-      } else {
-        throw new HttpsError(
-          "failed-precondition",
-          "SMTP transport failed verification",
-        );
+    const client = (() => {
+      try {
+        return new MailtrapClient({
+          token: mailtrapToken.value(),
+        });
+      } catch (err) {
+        if (err) {
+          throw new HttpsError("unknown", err.toString(), err);
+        } else {
+          throw new HttpsError("unknown", "Failed to create mail transport");
+        }
       }
-    }
+    })();
 
-    const message = {
-      sender: sender.value(),
-      from: { name: request.data.name, address: request.data.email },
-      to: to.value(),
+    // RFC 4021 §2.1.2–3 and RFC 822 §4.4.1–2 clearly state that, if `From`
+    // and `Sender` are both present, then `From` identifies the author
+    // (the agent who composed the message and wishes it to be sent),
+    // whereas `Sender` identifies the sender (the authenticated agent
+    // responsible for transmitting the message to the receiver).
+    // In other words, according to the standards, we should set
+    // the `From` address to `request.data.email` and the `Sender` address
+    // to `sender.value()`.
+    //
+    // However, Mailtrap apparently ignores the `Sender` header and rejects
+    // the message with an `Unauthorized` response if the `From` address
+    // doesn't match the Mailtrap account's sending domain.  That is,
+    // it apparently treats the `From` identity as the sender
+    // even if a `Sender` is explicitly specified.
+    //
+    // Therefore, contrary to the standard, we set the `from` address
+    // to the sender's address (because Mailtrap demands it) and omit
+    // the `sender` altogether (because Mailtrap ignores it).  In order
+    // to ensure that replies are addressed to the author rather than
+    // to the sender, we include the author's name and address in `replyTo`.
+    const message: Mail = {
+      from: { name: request.data.name, email: sender.value() },
+      to: [{ email: to.value() }],
+      headers: { "Reply-To": `${request.data.name} <${request.data.email}>` },
       subject: request.data.subject,
       text: request.data.body,
     };
 
     try {
-      await transport.sendMail(message);
+      await client.send(message);
     } catch (err) {
       if (err) {
         throw new HttpsError("unknown", err.toString(), err);
       } else {
-        throw new HttpsError("unknown", "Failed to send message");
+        throw new HttpsError("unknown", "Failed to send mail");
       }
     }
 
